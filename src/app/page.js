1372046fc31ab1abebcab3ca3f4ca2e7cf9b1d53
Mike <mike@Mikes-MBP-2.broadband>
2023-05-20 23:25:46 +0100

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,180 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useRef, useEffect } from "react";
+import styles from "./page.module.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function Home() {
+  const threeRef = useRef(null);
+  const fourRef = useRef(null);
+  const fiveRef = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.create({
+      trigger: threeRef.current,
+      start: "top top",
+      endTrigger: fourRef.current,
+      end: "top top",
+      pin: true,
+      pinSpacing: false,
+      onToggle: (self) => console.log("Is active", self.isActive),
+    });
+    ScrollTrigger.create({
+      trigger: fourRef.current,
+      start: "top top",
+      endTrigger: fiveRef.current,
+      end: "top top",
+      pin: true,
+      pinSpacing: false,
+      onToggle: (self) => console.log("Is active", self.isActive),
+    });
+    ScrollTrigger.create({
+      trigger: fiveRef.current,
+      start: "top top",
+      endTrigger: fiveRef.current,
+      end: "bottom bottom",
+      pin: true,
+      pinSpacing: false,
+      onToggle: (self) => console.log("Is active", self.isActive),
+    });
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className={styles.one}>
+        <div className={styles.content}>
+          <h3>Section One</h3>
+          <p>
+            Nulla congue venenatis feugiat. Nunc rutrum varius dapibus.
+            Suspendisse eu metus erat. Vestibulum id diam quam. Sed quis ornare
+            augue. Quisque consequat faucibus risus vitae sollicitudin. Nunc
+            lacinia, ipsum ut facilisis pharetra, velit dui venenatis eros, sit
+            amet interdum velit nisi at tortor. Nulla maximus magna suscipit,
+            feugiat nisi in, faucibus turpis. Maecenas ut faucibus tortor, id
+            lacinia est. Sed semper purus pharetra, cursus quam ac, bibendum
+            dolor. Integer massa magna, venenatis in elit ac, suscipit varius
+            massa. Nullam quis nisi pharetra, rhoncus quam hendrerit, tincidunt
+            nisl. Nullam porta est blandit finibus vulputate. Duis id risus a
+            odio venenatis pharetra sit amet at nisi. In hac habitasse platea
+            dictumst. Ut id tincidunt tellus.
+          </p>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+      <div className={styles.two}>
+        <div className={styles.content}>
+          <h3>Section Two</h3>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Nulla congue venenatis feugiat. Nunc rutrum varius dapibus.
+            Suspendisse eu metus erat. Vestibulum id diam quam. Sed quis ornare
+            augue. Quisque consequat faucibus risus vitae sollicitudin. Nunc
+            lacinia, ipsum ut facilisis pharetra, velit dui venenatis eros, sit
+            amet interdum velit nisi at tortor. Nulla maximus magna suscipit,
+            feugiat nisi in, faucibus turpis. Maecenas ut faucibus tortor, id
+            lacinia est. Sed semper purus pharetra, cursus quam ac, bibendum
+            dolor. Integer massa magna, venenatis in elit ac, suscipit varius
+            massa. Nullam quis nisi pharetra, rhoncus quam hendrerit, tincidunt
+            nisl. Nullam porta est blandit finibus vulputate. Duis id risus a
+            odio venenatis pharetra sit amet at nisi. In hac habitasse platea
+            dictumst. Ut id tincidunt tellus.
           </p>
-        </a>
+        </div>
       </div>
-    </main>
-  )
+      <div ref={threeRef} className={styles.three}>
+        <div className={styles.content}>
+          <h3>Section Three</h3>
+          <p>
+            Nulla congue venenatis feugiat. Nunc rutrum varius dapibus.
+            Suspendisse eu metus erat. Vestibulum id diam quam. Sed quis ornare
+            augue. Quisque consequat faucibus risus vitae sollicitudin. Nunc
+            lacinia, ipsum ut facilisis pharetra, velit dui venenatis eros, sit
+            amet interdum velit nisi at tortor. Nulla maximus magna suscipit,
+            feugiat nisi in, faucibus turpis. Maecenas ut faucibus tortor, id
+            lacinia est. Sed semper purus pharetra, cursus quam ac, bibendum
+            dolor. Integer massa magna, venenatis in elit ac, suscipit varius
+            massa. Nullam quis nisi pharetra, rhoncus quam hendrerit, tincidunt
+            nisl. Nullam porta est blandit finibus vulputate. Duis id risus a
+            odio venenatis pharetra sit amet at nisi. In hac habitasse platea
+            dictumst. Ut id tincidunt tellus.
+          </p>
+        </div>
+      </div>
+      <div ref={fourRef} className={styles.four}>
+        <div className={styles.content}>
+          <h3>Section Four</h3>
+          <p>
+            Nulla congue venenatis feugiat. Nunc rutrum varius dapibus.
+            Suspendisse eu metus erat. Vestibulum id diam quam. Sed quis ornare
+            augue. Quisque consequat faucibus risus vitae sollicitudin. Nunc
+            lacinia, ipsum ut facilisis pharetra, velit dui venenatis eros, sit
+            amet interdum velit nisi at tortor. Nulla maximus magna suscipit,
+            feugiat nisi in, faucibus turpis. Maecenas ut faucibus tortor, id
+            lacinia est. Sed semper purus pharetra, cursus quam ac, bibendum
+            dolor. Integer massa magna, venenatis in elit ac, suscipit varius
+            massa. Nullam quis nisi pharetra, rhoncus quam hendrerit, tincidunt
+            nisl. Nullam porta est blandit finibus vulputate. Duis id risus a
+            odio venenatis pharetra sit amet at nisi. In hac habitasse platea
+            dictumst. Ut id tincidunt tellus.
+          </p>
+        </div>
+      </div>
+      <div ref={fiveRef} className={styles.five}>
+        <div className={styles.content}>
+          <h3>Section Five</h3>
+          <p>
+            Nulla congue venenatis feugiat. Nunc rutrum varius dapibus.
+            Suspendisse eu metus erat. Vestibulum id diam quam. Sed quis ornare
+            augue. Quisque consequat faucibus risus vitae sollicitudin. Nunc
+            lacinia, ipsum ut facilisis pharetra, velit dui venenatis eros, sit
+            amet interdum velit nisi at tortor. Nulla maximus magna suscipit,
+            feugiat nisi in, faucibus turpis. Maecenas ut faucibus tortor, id
+            lacinia est. Sed semper purus pharetra, cursus quam ac, bibendum
+            dolor. Integer massa magna, venenatis in elit ac, suscipit varius
+            massa. Nullam quis nisi pharetra, rhoncus quam hendrerit, tincidunt
+            nisl. Nullam porta est blandit finibus vulputate. Duis id risus a
+            odio venenatis pharetra sit amet at nisi. In hac habitasse platea
+            dictumst. Ut id tincidunt tellus.
+          </p>
+        </div>
+      </div>
+      <div className={styles.six}>
+        <div className={styles.content}>
+          <h3>Section Six</h3>
+          <p>
+            Nulla congue venenatis feugiat. Nunc rutrum varius dapibus.
+            Suspendisse eu metus erat. Vestibulum id diam quam. Sed quis ornare
+            augue. Quisque consequat faucibus risus vitae sollicitudin. Nunc
+            lacinia, ipsum ut facilisis pharetra, velit dui venenatis eros, sit
+            amet interdum velit nisi at tortor. Nulla maximus magna suscipit,
+            feugiat nisi in, faucibus turpis. Maecenas ut faucibus tortor, id
+            lacinia est. Sed semper purus pharetra, cursus quam ac, bibendum
+            dolor. Integer massa magna, venenatis in elit ac, suscipit varius
+            massa. Nullam quis nisi pharetra, rhoncus quam hendrerit, tincidunt
+            nisl. Nullam porta est blandit finibus vulputate. Duis id risus a
+            odio venenatis pharetra sit amet at nisi. In hac habitasse platea
+            dictumst. Ut id tincidunt tellus.
+          </p>
+        </div>
+      </div>
+      <div className={styles.seven}>
+        <div className={styles.content}>
+          <h3>Section Seven</h3>
+          <p>
+            Nulla congue venenatis feugiat. Nunc rutrum varius dapibus.
+            Suspendisse eu metus erat. Vestibulum id diam quam. Sed quis ornare
+            augue. Quisque consequat faucibus risus vitae sollicitudin. Nunc
+            lacinia, ipsum ut facilisis pharetra, velit dui venenatis eros, sit
+            amet interdum velit nisi at tortor. Nulla maximus magna suscipit,
+            feugiat nisi in, faucibus turpis. Maecenas ut faucibus tortor, id
+            lacinia est. Sed semper purus pharetra, cursus quam ac, bibendum
+            dolor. Integer massa magna, venenatis in elit ac, suscipit varius
+            massa. Nullam quis nisi pharetra, rhoncus quam hendrerit, tincidunt
+            nisl. Nullam porta est blandit finibus vulputate. Duis id risus a
+            odio venenatis pharetra sit amet at nisi. In hac habitasse platea
+            dictumst. Ut id tincidunt tellus.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
